ca8ee37adf24c30199b25e5cc621b49f1e620cd2
rahulcj02 <cjrahul0502@gmail.com>
2025-07-06 18:14:49 -0500

    first Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import  Labs  from './Labs/index.tsx';
+
+function App() {
+return(
+<div>
+  <h1>Hello world</h1>
+  <Labs />
+</div>
+);
+}
+export default App
---
+++ src/Labs/index.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Labs () {
+  return (
+    <div>
+      <h1>Labs</h1>
+      <p>Welcome to the labs section!</p>
+    </div>
+  );
+}
