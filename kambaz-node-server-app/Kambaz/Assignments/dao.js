5c61f6aef37b85a4104c47f64b4e00f9f750c9cb
rahulcj02 <cjrahul0502@gmail.com>
2025-08-11 19:13:46 -0500

    assignment 6 compelete

--- kambaz-node-server-app/Kambaz/Assignments/dao.js
+++ kambaz-node-server-app/Kambaz/Assignments/dao.js
@@ -1,31 +1,24 @@
-// kambaz-node-server-app/Kambaz/Courses/Assignments/dao.js
-import Database from "../../Database/index.js";
+// kambaz-node-server-app/Kambaz/Assignments/dao.js
+import model from "./model.js";
 import { v4 as uuidv4 } from "uuid";
 
-export function findAssignmentsForCourse(courseId) {
-  const { assignments } = Database;
-  return assignments.filter(a => a.course === courseId);
+export async function findAssignmentsForCourse(courseId) {
+  return model.find({ course: courseId });
 }
 
-export function findAssignmentById(aid) {
-  const { assignments } = Database;
-  return assignments.find(a => a._id === aid);
+export async function findAssignmentById(aid) {
+  return model.findById(aid);
 }
 
-export function createAssignment(a) {
-  const newA = { _id: uuidv4(), ...a };
-  Database.assignments.push(newA);
-  return newA;
+export async function createAssignment(a) {
+  const toCreate = { ...a, _id: a._id || uuidv4() }; 
+  return model.create(toCreate);
 }
 
-export function updateAssignment(aid, updates) {
-  const { assignments } = Database;
-  const a = assignments.find(x => x._id === aid);
-  Object.assign(a, updates);
-  return a;
+export async function updateAssignment(aid, updates) {
+  return model.findByIdAndUpdate(aid, { $set: updates }, { new: true });
 }
 
-export function deleteAssignment(aid) {
-  const { assignments } = Database;
-  Database.assignments = assignments.filter(x => x._id !== aid);
+export async function deleteAssignment(aid) {
+  return model.deleteOne({ _id: aid });
 }
--- kambaz-node-server-app/Kambaz/Assignments/routes.js
+++ kambaz-node-server-app/Kambaz/Assignments/routes.js
@@ -2,34 +2,34 @@
 import * as dao from "./dao.js";
 
 export default function AssignmentRoutes(app) {
-  // get all for a course
-  app.get("/api/courses/:courseId/assignments", (req, res) => {
-    res.json(dao.findAssignmentsForCourse(req.params.courseId));
+  // get all assignments for a course
+  app.get("/api/courses/:courseId/assignments", async (req, res) => {
+    const assignments = await dao.findAssignmentsForCourse(req.params.courseId);
+    res.json(assignments);
   });
 
   // create
-  app.post("/api/assignments", (req, res) => {
-    const a = { ...req.body };
-    const newA = dao.createAssignment(a);
-    res.json(newA);
+  app.post("/api/assignments", async (req, res) => {
+    const created = await dao.createAssignment(req.body);
+    res.json(created);
   });
 
   // read one
-  app.get("/api/assignments/:aid", (req, res) => {
-    const a = dao.findAssignmentById(req.params.aid);
+  app.get("/api/assignments/:aid", async (req, res) => {
+    const a = await dao.findAssignmentById(req.params.aid);
     if (!a) return res.sendStatus(404);
     res.json(a);
   });
 
   // update
-  app.put("/api/assignments/:aid", (req, res) => {
-    const updated = dao.updateAssignment(req.params.aid, req.body);
+  app.put("/api/assignments/:aid", async (req, res) => {
+    const updated = await dao.updateAssignment(req.params.aid, req.body);
     res.json(updated);
   });
 
   // delete
-  app.delete("/api/assignments/:aid", (req, res) => {
-    dao.deleteAssignment(req.params.aid);
+  app.delete("/api/assignments/:aid", async (req, res) => {
+    await dao.deleteAssignment(req.params.aid);
     res.sendStatus(204);
   });
 }
